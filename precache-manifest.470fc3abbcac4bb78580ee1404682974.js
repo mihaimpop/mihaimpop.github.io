5972954a2e3bd9efb1a98d833df8db0de24c0ab1
Mihai Pop <mihai.m.m.pop@gmail.com>
2019-05-05 21:22:32 +0300

    Updates

---
+++ precache-manifest.470fc3abbcac4bb78580ee1404682974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa7f16f6b0bfcd3b2a90ba101073cf0",
+    "url": "/personal-landing-page/index.html"
+  },
+  {
+    "revision": "176df43ebd3ce4d7a9c9",
+    "url": "/personal-landing-page/static/css/main.2d3dbafe.chunk.css"
+  },
+  {
+    "revision": "c7d4b8d18449bcb4cd8a",
+    "url": "/personal-landing-page/static/js/2.616303a6.chunk.js"
+  },
+  {
+    "revision": "176df43ebd3ce4d7a9c9",
+    "url": "/personal-landing-page/static/js/main.f410261d.chunk.js"
+  },
+  {
+    "revision": "84742de814e8c47a4cc2",
+    "url": "/personal-landing-page/static/js/runtime~main.e46c6189.js"
+  },
+  {
+    "revision": "417844c6e931a645ff9d9e06ff26abc1",
+    "url": "/personal-landing-page/static/media/bg.417844c6.png"
+  },
+  {
+    "revision": "1e9132b07adc03f15508cdcdc51d4dd0",
+    "url": "/personal-landing-page/static/media/profile.1e9132b0.jpg"
+  }
+]);
