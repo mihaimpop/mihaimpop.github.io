42a3aa3f8743b6487fbb668343024405e61adafd
Mihai Pop <mihai.m.m.pop@gmail.com>
2019-05-05 21:28:27 +0300

    Personal landing page v2

---
+++ precache-manifest.45457243604f48fe7656a6670e9b733b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea75979b0170f1b0bded05f95bbc5813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ad83e5391d35e68274e",
+    "url": "/static/css/main.b2943749.chunk.css"
+  },
+  {
+    "revision": "c7d4b8d18449bcb4cd8a",
+    "url": "/static/js/2.616303a6.chunk.js"
+  },
+  {
+    "revision": "2ad83e5391d35e68274e",
+    "url": "/static/js/main.79b766ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "417844c6e931a645ff9d9e06ff26abc1",
+    "url": "/static/media/bg.417844c6.png"
+  },
+  {
+    "revision": "1e9132b07adc03f15508cdcdc51d4dd0",
+    "url": "/static/media/profile.1e9132b0.jpg"
+  }
+]);
