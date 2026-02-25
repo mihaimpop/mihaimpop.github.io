ff37320c1d79c369241d12e6551f687a73f3a5cb
Mihai Pop <mihai.m.m.pop@gmail.com>
2026-02-25 22:12:24 +0200

    fix: missing pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
   server: {
     port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(process.cwd(), "index.html"),
+        mihaiPop: resolve(process.cwd(), "mihai-pop.html"),
+        mihaiPopFilm: resolve(process.cwd(), "mihai-pop-film.html"),
+        mihaiPopYoutube: resolve(process.cwd(), "mihai-pop-youtube.html"),
+      },
+    },
   },
 });
